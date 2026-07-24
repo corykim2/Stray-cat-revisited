3d5280582b32c1235dd7e5d6adad8e6f1be41f00
corykim2 <sos2591@naver.com>
2026-07-25 00:35:55 +0900

    fix: 배포 환경에서 정적 파일 서빙 및 운영 서버 전환
    개발 서버가 암묵적으로 처리하던 것들을 명시적으로 지정.
    
    - whitenoise 도입, 빌드 시점 collectstatic 수행
    - static() 헬퍼는 DEBUG=False에서 빈 리스트를 반환해 serve로 대체
    - runserver -> gunicorn (worker 2)
    
    DEBUG=False로 배포한 뒤에야 CSS가 전혀 적용되지 않는 것을 발견.
    runserver가 DEBUG=True일 때만 제공하던 기능에 의존하고 있었음."

--- cat_project/config/urls.py
+++ cat_project/config/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.conf import settings
+from django.urls import re_path
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -24,4 +26,8 @@ urlpatterns = [
     path('', include('account.urls')),
     path('account/', include('account.urls')),
     path('cat_inf/', include('cat_inf.urls')),
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
