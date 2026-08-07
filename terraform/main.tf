6d9520d8dabc23354e9276e27e9d47b60dcabd73
corykim2 <sos2591@naver.com>
2026-08-07 21:10:47 +0900

    feat: Terraform으로 GCP VM 인프라 코드화

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+﻿terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "straycat-503411"
+  region  = "asia-northeast3"
+  zone    = "asia-northeast3-a"
+}
+
+resource "google_compute_instance" "practice" {
+  name         = "terraform-practice"
+  machine_type = "e2-micro"
+  zone         = "asia-northeast3-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
